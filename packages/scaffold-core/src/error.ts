41f0b09fcb1cc64c1099e4b45cec4cdc1520754a
Patrick Lam <zidizei@gmail.com>
2019-06-03 14:41:28 +0200

    feat(core): define name for custom Errors

--- packages/scaffold-core/src/error.ts
+++ packages/scaffold-core/src/error.ts
@@ -2,6 +2,8 @@ import path from "path"
 
 export class LoadFromUrlError extends Error {
 
+    name = "LoadFromUrlError"
+
     constructor(url: string) {
         super(`Failed to load Scaffold from URL '${url}'.`)
     }
@@ -9,6 +11,8 @@ export class LoadFromUrlError extends Error {
 }
 
 export class LoadFromFileError extends Error {
+
+    name = "LoadFromFileError"
 
     constructor(url: string, filePath: string) {
         super(`Failed to load Scaffold from URL '${url}' from file '${path.basename(filePath)}'.`)
